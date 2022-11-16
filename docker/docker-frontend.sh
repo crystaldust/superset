03b45b49e678ba97acc6e1a004138ab19d383ff0
Zhen Ju <juzhenatpku@gmail.com>
2022-11-16 15:48:21 +0800

    Make superset easier to launch with docker compose

--- docker/docker-frontend.sh
+++ docker/docker-frontend.sh
@@ -22,8 +22,9 @@ apt update
 apt install -y chromium
 
 cd /app/superset-frontend
+npm config set registry https://registry.npm.taobao.org
 npm install -f --no-optional --global webpack webpack-cli
-npm install -f --no-optional
+npm install --no-optional
 
 echo "Running frontend"
 npm run dev
